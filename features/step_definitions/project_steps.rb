22f0970357395291d68d8eb36f3a98e3ccba7af7
javafun <mscsharp@gmail.com>
2013-04-01 22:49:52 +1100

    Added title functionality for show page

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -24,15 +24,15 @@ Given /^(?:|I )am on the (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When(/^I follow "(.*?)"$/) do |link|
+When /^I follow "(.*?)"$/ do |link|
   click_link(link)
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
+When /^I fill in "(.*?)" with "(.*?)"$/ do |field, value|
   fill_in(field, :with => value)
 end
 
-When(/^I press "(.*?)"$/) do |button|
+When /^I press "(.*?)"$/ do |button|
   click_button(button)
 end
 
@@ -43,3 +43,16 @@ Then(/^I should see "(.*?)"$/) do |text|
     assert page.has_content?(text)
   end
 end
+
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
+
+
+
--- features/support/path.rb
+++ features/support/path.rb
@@ -3,6 +3,8 @@ module NavigationHelpers
     case page_name
     when /the home\s?page/
       '/'
+    when /the project page for "([^\"]*)"/
+      project_path(Project.find_by_name!($1))
     when /new task/i
       new_task_path
     when /tasks/i
